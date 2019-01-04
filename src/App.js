f67a37c506a5b526955b685d4123e35e70cf3936
Unknown <guillaume.chevriergros@gmail.com>
2019-01-04 17:16:53 +0100

    Access database on firebase

--- src/App.js
+++ src/App.js
@@ -2,23 +2,49 @@ import React, { Component } from 'react';
 import logo from './yin_yang.png';
 import ReactPlayer from 'react-player';
 import './App.css';
+import * as firebase from 'firebase'
+
+var config = {
+  apiKey: "AIzaSyARWBPFqoqI5mtu1fh_QSJMCRLM5564ioA",
+  authDomain: "relaxa-93e3b.firebaseapp.com",
+  databaseURL: "https://relaxa-93e3b.firebaseio.com",
+  projectId: "relaxa-93e3b",
+  storageBucket: "relaxa-93e3b.appspot.com",
+  messagingSenderId: "587203721788"
+};
 
 class App extends Component {
-  state = {
-    url: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
-    pip: false,
-    playing: true,
-    volume: 0.5,
-    muted: false,
-    played: 0,
-    loaded: 0,
-    duration: 0,
-    playbackRate: 1.0,
-    loop: false
+  constructor ()
+  {
+    super()
+    this.state = {
+      url: "https://firebasestorage.googleapis.com/v0/b/relaxa-93e3b.appspot.com/o/compo9.mp3?alt=media&token=15dd4de6-a601-41ff-b8cc-82db97fc0493",
+      pip: false,
+      playing: true,
+      volume: 0.5,
+      muted: false,
+      played: 0,
+      loaded: 0,
+      duration: 0,
+      playbackRate: 1.0,
+      loop: false,
+      content: null
+    }
+    this.ref = player => {
+      this.player = player
+    }
+    firebase.initializeApp(config);
   }
 
-  ref = player => {
-    this.player = player
+  componentWillMount() {
+    console.log("component will mount");
+    const refe = firebase.database().ref('musics')
+    console.log(refe);
+    refe.on('value', snapshot => {
+      console.log('on value...')
+      console.log(snapshot.val);
+      this.setState({content: snapshot.val()});
+    })
   }
 
   volume_up() {
@@ -45,7 +71,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to relaxa. It change over the time !
+            Welcome to relaxa. Let's play zen music !
           </p>
           <ReactPlayer ref={this.ref} 
                         url={url} 
@@ -58,6 +84,7 @@ class App extends Component {
           <button onClick={() => this.playPause()}>Play/Pause</button>
           <button onClick={() => this.volume_up()}>+</button>
           <button onClick={() => this.volume_down()}>-</button>
+          <p> database content: {this.content}</p>
         </header>
       </div>
     );
